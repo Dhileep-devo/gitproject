ef57f82c98e68e0d252c321d46e7e5baf9e3b397
dmunige <82717168+dmunige@users.noreply.github.com>
2021-05-28 14:19:27 +0530

    urls 1.o

--- gitproject/gitproject/urls.py
+++ gitproject/gitproject/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from myapp.views import views_login
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views_login)
 ]
