98286647463380b762021696bd0acee9c79c6100
dmunige <82717168+dmunige@users.noreply.github.com>
2021-05-31 19:22:41 +0530

    view

--- gitproject/myapp/views.py
+++ gitproject/myapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .forms import studentForm
 
 # Create your views here.
+# @api_view(['get','post'])
 def views_login(request):
-    return render(request,'login.html')
+    form=studentForm()
+    if(request.method=='GET'):
+        return render(request,'login.html',{'form':form})
+    print(request.FILES)
+    form=studentForm(request.POST,request.FILES)
+    print(form)
+    print(form.cleaned_data)
+    if(form.is_valid()):
+        form.save()
+    return render(request,'login.html',{'form':form})
